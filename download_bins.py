3195a7050b5cdba15466bbad6c81e4b006aad6c5
monsivamon <brackdocomo@outlook.com>
2026-07-14 19:33:46 +0900

    Update download_morphe_cli to download desktop version

--- download_bins.py
+++ download_bins.py
@@ -59,8 +59,8 @@ def download_apkeditor():
 def download_morphe_cli():
     print("Downloading Morphe CLI...")
     download_release_asset(
-        "MorpheApp/morphe-cli",
-        r".*morphe-cli.*-all\.jar$",
+        "MorpheApp/morphe-desktop",
+        r".*morphe-desktop.*-all\.jar$",
         "bins",
         "morphe-cli.jar",
         include_prereleases=True
